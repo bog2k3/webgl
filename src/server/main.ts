4aa3505f828134c0ce169102a48e17b8d7b308e9
Bogdan Ionita <bog2k3@gmail.com>
2023-03-04 18:52:51 +0200

    +http server & socket io

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,20 @@
+const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+
 (function main(): void {
-	console.log("server works");
+	const app = express();
+	const server = http.createServer(app);
+	const socketIo = new Server(server);
+	socketIo.on("connection", (socket) => {
+		console.log("socket connected:", socket);
+	});
+
+	app.get("/", (req, res) => {
+		res.sendFile(__dirname + "../../index.html");
+	});
+
+	server.listen(3000, () => {
+		console.log("Listening on HTTP *:3000");
+	});
 })();
