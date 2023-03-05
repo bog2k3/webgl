2aeba1329b2a478f366bac5b91836fddded6e6fc
Bogdan Ionita <bog2k3@gmail.com>
2023-03-05 18:17:12 +0200

    serving the client via own server

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,17 +1,31 @@
 const { Server } = require("socket.io");
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 
 (function main(): void {
 	const app = express();
+	app.use(cors({ origin: "*" }));
 	const server = http.createServer(app);
 	const socketIo = new Server(server);
 	socketIo.on("connection", (socket) => {
 		console.log("socket connected:", socket);
 	});
 
+	const fileOptions = {
+		root: __dirname + "/../..",
+	};
+
+	app.get("/dist/*", (req, res) => {
+		res.sendFile(req.url, fileOptions);
+	});
+
+	app.get("/data/*", (req, res) => {
+		res.sendFile(req.url, fileOptions);
+	});
+
 	app.get("/", (req, res) => {
-		res.sendFile(__dirname + "../../index.html");
+		res.sendFile("index.html", fileOptions);
 	});
 
 	server.listen(3000, () => {
