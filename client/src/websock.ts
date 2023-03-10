8465849ad003be9be706fde5422b49ecbe9d2d08
bogdan ionita <bog2k3@gmail.com>
2023-03-10 17:28:13 +0200

    fixed server configuration

--- client/src/websock.ts
+++ client/src/websock.ts
@@ -1,4 +1,5 @@
 import { io, Socket } from "socket.io-client";
+import { SocketMessage } from "./common/socket-message";
 
 let socket: Socket;
 
@@ -6,5 +7,5 @@ export function initWebSocket(): void {
 	socket = io({
 		host: "localhost:3000/socket.io",
 	});
-	socket.send("hello");
+	socket.send(SocketMessage.IDENTIFY, { name: "vasile" });
 }
